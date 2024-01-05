33f36fc67f525321ce70f06cbdccfb913887991b
Julian Bogdani <jbogdani@gmail.com>
2024-01-05 22:35:29 +0100

    personalizzazione

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Directus Gatsby UI`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
