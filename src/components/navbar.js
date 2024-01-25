ff0af420a5588bfa2cb28b5912ff0b59fc67ced2
Julian Bogdani <jbogdani@gmail.com>
2024-01-25 07:09:37 +0100

    Sistemati link nel menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,12 +7,11 @@ function MyNavbar(props) {
     <Menu>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">{props.siteTitle}</Navbar.Brand>
+          <Navbar.Brand href="/">{props.siteTitle}</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/home">Home</Nav.Link>
-              <Nav.Link href="/page-2">Static page</Nav.Link>
+                                                                                                                                                                                                <Nav.Link href="/page-2">Static page</Nav.Link>
               <Nav.Link href="/searchPage">Search</Nav.Link>
               <Nav.Link href="/datatable">Datatable</Nav.Link>
               <Nav.Link href="/mappa-json">Map GeoJson data</Nav.Link>
