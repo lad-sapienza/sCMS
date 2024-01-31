fe2cd126b72da8b16741aeaa0005cfa5bdb6a862
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2024-01-31 03:06:49 +0100

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,8 +11,8 @@ function MyNavbar(props) {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/home">Home</Nav.Link>
-              <Nav.Link href="/page-2">Static page</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/src/contents/test.mdx">Page MDX</Nav.Link>
               <Nav.Link href="/searchPage">Search</Nav.Link>
               <Nav.Link href="/datatable">Datatable</Nav.Link>
               <Nav.Link href="/mappa-json">Map GeoJson data</Nav.Link>
