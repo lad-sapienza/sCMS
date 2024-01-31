8a5790bd5062c868274276b4e117609719d50527
Julian Bogdani <jbogdani@gmail.com>
2024-01-31 22:43:12 +0100

    Link href sostituito con Link to

--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -52,7 +52,7 @@ const SearchPage = () => {
       <br />
       <br />
       <br />
-      <Link href="/">Torna alla homepage</Link>
+      <Link to="/">Torna alla homepage</Link>
     </Layout>
   )
 }
