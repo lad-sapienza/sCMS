34d033ac43a244d0f3bf86b4c75b7c1902f94803
Julian Bogdani <jbogdani@gmail.com>
2024-02-01 15:21:32 +0100

    fixed image missing

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/scms.png`, // This path is relative to the root of the site.
+        icon: `src/images/lad-rect.png`, // This path is relative to the root of the site.
       },
     },
     {
