7cada604bc34b3342278d46e2f214575e3d28598
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2024-02-03 06:34:45 +0100

    Update datatb.js

--- src/modules/datatb.js
+++ src/modules/datatb.js
@@ -13,6 +13,7 @@ const DataTb = props => {
   // Stato per gestire lo stato di errore
   const [errore, impostaErrore] = useState(null)
   const [searchText, setSearchText] = useState("")
+  const [debounceTimer, setDebounceTimer] = useState(null)
   const [dataLimit] = useState(props.dLimit || 100)
 
   // Dependency check
@@ -26,44 +27,62 @@ const DataTb = props => {
   if (props.dTable && !props.dToken) {
     impostaErrore({ message: "Directus token is missing" })
   }
+  const handleSearch = e => {
+    const searchTerm = e.target.value
+    setSearchText(searchTerm)
+
+    // Cancella il timer precedente
+    if (debounceTimer) {
+      clearTimeout(debounceTimer)
+    }
+
+    // Imposta un nuovo timer per eseguire la ricerca dopo 300 millisecondi
+    const newDebounceTimer = setTimeout(() => {
+      setDebounceTimer(null)
+      handleDebouncedSearch(searchTerm)
+    }, 300)
+
+    setDebounceTimer(newDebounceTimer)
+  }
+
+  const handleDebouncedSearch = searchTerm => {
+    setSearchText(searchTerm)
+  }
 
   // useEffect per ottenere dati quando il componente viene montato
   useEffect(() => {
     const ottieniDati = async page => {
-      const offset = (page - 1) * dataLimit
-      try {
-        // Imposta lo stato di caricamento a true durante il recupero dei dati
-        impostaCaricamento(true)
-        // Ottieni i dati dall'API
-        const risposta = await fetch(
-          `${props.dTable}?limit=${dataLimit}&offset=${offset}`,
-          {
-            headers: {
-              Authorization: `Bearer ${props.dToken}`, // Aggiungi il token all'header
-            },
-          }
-        )
-        // Parsa la risposta JSON
-        const risultato = await risposta.json()
-        // Aggiorna lo stato con i dati ottenuti
-        impostaDati(risultato.data || [])
-      } catch (errore) {
-        // Se si verifica un errore, aggiorna lo stato di errore
-        impostaErrore(errore)
-      } finally {
-        // Imposta lo stato di caricamento a false quando il recupero è completato
-        impostaCaricamento(false)
+      if (!debounceTimer) {
+        const offset = (page - 1) * dataLimit
+        try {
+          // Esegui la ricerca solo quando il timer di debounce è scaduto
+          // Imposta lo stato di caricamento a true durante il recupero dei dati
+          impostaCaricamento(true)
+          // Ottieni i dati dall'API
+          const risposta = await fetch(
+            `${props.dTable}?limit=${dataLimit}&offset=${offset}`,
+            {
+              headers: {
+                Authorization: `Bearer ${props.dToken}`, // Aggiungi il token all'header
+              },
+            }
+          )
+          // Parsa la risposta JSON
+          const risultato = await risposta.json()
+          // Aggiorna lo stato con i dati ottenuti
+          impostaDati(risultato.data || [])
+        } catch (errore) {
+          // Se si verifica un errore, aggiorna lo stato di errore
+          impostaErrore(errore)
+        } finally {
+          // Imposta lo stato di caricamento a false quando il recupero è completato
+          impostaCaricamento(false)
+        }
       }
     }
-
     // Chiama la funzione ottieniDati quando il componente viene montato e inserisci il valore iniziale della pagina
     ottieniDati(1)
   }, [props, dataLimit, errore, searchText]) // L'array di dipendenze vuoto assicura che questo effetto venga eseguito solo una volta, simile a componentDidMount
-
-  const handleSearch = e => {
-    const searchTerm = e.target.value
-    setSearchText(searchTerm)
-  }
 
   const filteredData = dati.filter(item =>
     Object.values(item).some(
