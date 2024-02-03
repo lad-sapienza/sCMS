d974ebf0e3f0c1e50e56474fe6c8feb08cbb8ee2
Julian Bogdani <jbogdani@gmail.com>
2024-02-03 15:30:24 +0100

    Implementd Seo in MDX generated pages

--- src/templates/contents.jsx
+++ src/templates/contents.jsx
@@ -3,7 +3,9 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { Container } from "react-bootstrap"
 import { MDXProvider } from "@mdx-js/react"
-//import { Mappa } from "../components/maps"
+
+import Seo from "../components/seo"
+
 
 const shortcodes = {} // Provide common components here
 
@@ -11,13 +13,14 @@ export default function PageTemplate({ data, children }) {
   return (
     <Layout>
       <Container>
-        <h1>{data.mdx.frontmatter.title}</h1>
-        <div>{data.mdx.frontmatter.description}</div>
         <MDXProvider components={shortcodes}>{children}</MDXProvider>
       </Container>
     </Layout>
   )
 }
+
+export const Head = ({data}) => <Seo title={data.mdx.frontmatter.title} description={data.mdx.frontmatter.description} />
+
 
 export const query = graphql`
   query ($id: String!) {
