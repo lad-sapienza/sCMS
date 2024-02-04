d7d5b6a10d0a58cbb0deb514da0e854103c8499e
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2024-02-05 00:19:59 +0100

    Update search.js

--- src/modules/search.js
+++ src/modules/search.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useState } from "react"
 import Seo from "../components/seo"
-import { Container } from "react-bootstrap"
 
 const Search = props => {
   const [query, setQuery] = useState("")
@@ -36,46 +35,44 @@ const Search = props => {
     }
   }
   return (
-    <Container>
-      <Fragment>
-        <Seo title="Ricerca" />
-        <form onSubmit={handleSubmit} className="mt-5 row">
-          <div class="col-auto">
-            <label htmlFor="search_input" class="visually-hidden">
-              Search
-            </label>
-            <input
-              id="search_input"
-              type="text"
-              className="form-control"
-              value={query}
-              onChange={e => setQuery(e.target.value)}
-              placeholder="Search the database for something"
-            />
-          </div>
+    <Fragment>
+      <Seo title="Ricerca" />
+      <form onSubmit={handleSubmit} className="mt-5 row">
+        <div class="col-auto">
+          <label htmlFor="search_input" class="visually-hidden">
+            Search
+          </label>
+          <input
+            id="search_input"
+            type="text"
+            className="form-control"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+            placeholder="Search the database for something"
+          />
+        </div>
 
-          <div class="col-auto">
-            <button type="submit" className="btn btn-primary">
-              Search
-            </button>
-          </div>
-        </form>
+        <div class="col-auto">
+          <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+        </div>
+      </form>
 
-        {searchResults && (
-          <Fragment>
-            <h1 className="mt-5">Risultati</h1>
-            <ul>
-              {searchResults.data.map((result, index) => (
-                <li key={index}>
-                  <h2>{result.title}</h2>
-                  <p>{result.summary}</p>
-                </li>
-              ))}
-            </ul>
-          </Fragment>
-        )}
-      </Fragment>
-    </Container>
+      {searchResults && (
+        <Fragment>
+          <h1 className="mt-5">Risultati</h1>
+          <ul>
+            {searchResults.data.map((result, index) => (
+              <li key={index}>
+                <h2>{result.title}</h2>
+                <p>{result.summary}</p>
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      )}
+    </Fragment>
   )
 }
 
