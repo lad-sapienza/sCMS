b8e550955e6eda78a7bb61780f4b430090bb7786
Julian Bogdani <jbogdani@gmail.com>
2024-02-05 10:31:37 +0100

    Added withPrefioc in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -25,7 +25,7 @@ function MyNavbar(props) {
     <Menu>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="/">{props.siteTitle}</Navbar.Brand>
+          <Navbar.Brand href={ withPrefix(`/`) }>{props.siteTitle}</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
