53b5911903e3b7069c35258603fdb5eaead0db4b
Julian Bogdani <jbogdani@gmail.com>
2024-02-12 10:00:06 +0100

    Enhanced record

--- src/modules/viewRecord.js
+++ src/modules/viewRecord.js
@@ -6,7 +6,7 @@ const Record = ( {item} ) => {
     {Object.entries(item).map((itemEl, index) => (
       <dl key={index}>
         <dt>{itemEl[0]}</dt>
-        <dd dangerouslySetInnerHTML={{ __html: itemEl[1] }}></dd>
+        <dd dangerouslySetInnerHTML={{ __html: typeof(itemEl[1]) === 'string' ? itemEl[1] : `<pre>${JSON.stringify(itemEl[1], null, 2)}</pre>` }}></dd>
       </dl>
     ))}
   </Fragment>
