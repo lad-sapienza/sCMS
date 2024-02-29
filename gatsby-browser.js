a6c1688f8eb04e7c25542b8175085e26e2ea936e
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2024-03-01 00:34:18 +0100

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,1 @@
 import "bootstrap/dist/js/bootstrap.min.js"
-
-export const onInitialClientRender = () => {
-  // Redirect logic
-  const currentPath = window.location.pathname
-
-  // Replace "/path" with the actual path you want to redirect from
-  if (currentPath === "/sCMS/") {
-    // Replace "/home" with the actual path you want to redirect to
-    window.location.replace("/sCMS/home")
-  }
-}
