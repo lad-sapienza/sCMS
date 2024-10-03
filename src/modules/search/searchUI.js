aca5702613ec75e7d0cad1c82b9853600d61b020
Julian Bogdani <jbogdani@gmail.com>
2024-10-03 11:58:02 +0200

    Updated icons

--- src/modules/search/searchUI.js
+++ src/modules/search/searchUI.js
@@ -117,10 +117,11 @@ const SearchUI = ({ fieldList, operators, connector }) => {
                   width="16"
                   height="16"
                   fill="currentColor"
-                  className="bi bi-trash3"
+                  className="bi bi-dash-circle"
                   viewBox="0 0 16 16"
                 >
-                  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
                 </svg>
               </Button>
             )}
@@ -136,10 +137,11 @@ const SearchUI = ({ fieldList, operators, connector }) => {
                     width="16"
                     height="16"
                     fill="currentColor"
-                    className="bi bi-plus-circle-fill"
+                    className="bi bi-plus-circle"
                     viewBox="0 0 16 16"
                   >
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                   </svg>
                 </Button>
                 <Button
@@ -173,7 +175,9 @@ const SearchUI = ({ fieldList, operators, connector }) => {
           </pre>
         </Col>
         <Col>
-        <p className="border mb-3 shadow p-2">Output ransformed to Directus API syntax:</p>
+          <p className="border mb-3 shadow p-2">
+            Output ransformed to Directus API syntax:
+          </p>
           <pre>
             <code language="js">
               {JSON.stringify(plain2directus(conn, inputs), null, 2)}
@@ -181,7 +185,9 @@ const SearchUI = ({ fieldList, operators, connector }) => {
           </pre>
         </Col>
         <Col>
-        <p className="border mb-3 shadow p-2">Output ransformed to MapLibre Expression syntax:</p>
+          <p className="border mb-3 shadow p-2">
+            Output ransformed to MapLibre Expression syntax:
+          </p>
           <pre>
             <code language="js">
               {JSON.stringify(plain2maplibre(conn, inputs), null, 2)}
