1b8802f3c40a1801e0e578c2abbcf91ebf864a00
Julian Bogdani <jbogdani@gmail.com>
2024-10-03 22:00:26 +0200

    minor bug fixed

--- src/modules/search.js
+++ src/modules/search.js
@@ -14,14 +14,13 @@ const Search = ({
   resultItemTemplate,
   fieldList,
   operators,
-  connector
+  connector,
 }) => {
   const [searchResults, setSearchResults] = useState(null)
   // Stato per gestire lo stato di errore
   const [error, setError] = useState(null)
 
   const processData = (conn, inputs) => {
-    
     let endPoint
 
     if (dEndPoint) {
@@ -29,30 +28,27 @@ const Search = ({
     } else if (dTable) {
       if (!process.env.GATSBY_DIRECTUS_ENDPOINT) {
         setError(
-          "Cannot calculate API end-point. Parameter dTable requires the enc variable GATSBY_DIRECTUS_ENDPOINT to  be set"
+          "Cannot calculate API end-point. Parameter dTable requires the enc variable GATSBY_DIRECTUS_ENDPOINT to  be set",
         )
       }
       endPoint = `${process.env.GATSBY_DIRECTUS_ENDPOINT}items/${dTable}`
     } else {
       setError(
-        "Cannot calculate API end-point. dEndpoint or dTable parameter is missing"
+        "Cannot calculate API end-point. dEndpoint or dTable parameter is missing",
       )
     }
 
-    const token = dToken
-        ? dToken
-        : process.env.GATSBY_DIRECTUS_TOKEN;
-    
+    const token = dToken ? dToken : process.env.GATSBY_DIRECTUS_TOKEN
 
     if (!token) {
       setError(
-        "Directus token is missing. It should be provided as dToken parameter or as a GATSBY_DIRECTUS_TOKEN env variable"
+        "Directus token is missing. It should be provided as dToken parameter or as a GATSBY_DIRECTUS_TOKEN env variable",
       )
     }
     if (!fieldList) {
       setError("fieldList parameter is mising")
     }
-    const filter = JSON.stringify(plain2directus(conn, inputs));
+    const filter = JSON.stringify(plain2directus(conn, inputs))
 
     getData(`${endPoint}?filter=${filter}`, token, "json")
       .then(data => {
@@ -60,7 +56,7 @@ const Search = ({
           console.log(data.errors)
           setError("Error in querying getting remote data 1")
         } else {
-          setError(null);
+          setError(null)
           setSearchResults(data)
         }
       })
@@ -72,14 +68,20 @@ const Search = ({
 
   return (
     <Fragment>
-      <SearchUI fieldList={fieldList} operators={operators} connector={connector} processData={processData} />
-      
+      <SearchUI
+        fieldList={fieldList}
+        operators={operators}
+        connector={connector}
+        processData={processData}
+      />
 
       {error && <div className="text-danger">{error}</div>}
-      
-      {searchResults.data.length ===0  && <div className="text-warning">No results found</div>}
 
-      {searchResults && searchResults.data.length > 0  && !error && (
+      {searchResults?.data.length === 0 && (
+        <div className="text-warning">No results found</div>
+      )}
+
+      {searchResults?.data.length > 0 && !error && (
         <Fragment>
           <h1 className="mt-5">Results</h1>
           <div className="resultsContainer">
