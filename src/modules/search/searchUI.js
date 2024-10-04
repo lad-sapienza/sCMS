ab7b0b296ce917424b539f5749d8bcfc24948834
Julian Bogdani <jbogdani@gmail.com>
2024-10-04 06:44:53 +0200

    Added check for required parameter

--- src/modules/search/searchUI.js
+++ src/modules/search/searchUI.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react"
+import PropTypes from "prop-types"
+
 import { Button } from "react-bootstrap"
 
 import SearchUiAdv from "./searchUiAdv"
 import SearchUiSimple from "./searchUiSimple"
 
-const SearchUI = ({ fieldList, processData, operators, connector }) => {
+const SearchUI = ({ fieldList, processData, operators, connectors }) => {
   const [isSimple, setIsSimple] = useState(true)
 
   const onClickSimple = () => setIsSimple(!isSimple)
+
+  if (!fieldList) {
+    return (
+      <div className="text-danger">
+        Parameter <code>fieldList</code> missing! Component{" "}
+        <code>SearchUI</code> cannot be initialised
+      </div>
+    )
+  }
 
   return (
     <React.Fragment>
@@ -46,12 +57,38 @@ const SearchUI = ({ fieldList, processData, operators, connector }) => {
         <SearchUiAdv
           fieldList={fieldList}
           operators={operators}
-          connector={connector}
+          connectors={connectors}
           processData={processData}
         />
       )}
     </React.Fragment>
   )
 }
+
+SearchUI.propTypes = {
+  /**
+   * Required object with list of fields (field_name: field Label) to use for searching
+   */
+  fieldList: PropTypes.object.isRequired,
+  /**
+   * Callback function to run on data
+   */
+  processData: PropTypes.func,
+
+  /**
+   * Object with connectors to implement UI in other languages
+   */
+  operators: PropTypes.object,
+
+  /**
+   * Object with connectors to implement UI in other languages
+   * Must implement {
+   * "_and": "some Label"
+   * "_or": "Some Label"
+   * }
+   */
+  connectors: PropTypes.object,
+}
+
 
 export default SearchUI
