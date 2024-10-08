519aa47a90c3ee60dbeacc1b6bfeec593edd12ea
Julian Bogdani <jbogdani@gmail.com>
2024-10-08 18:38:04 +0200

    Modal correctly showing

--- src/modules/maps/maplibre/controlPanel.js
+++ src/modules/maps/maplibre/controlPanel.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import styled from "styled-components"
-import { Modal, Button } from "react-bootstrap"
+import { Modal } from "react-bootstrap"
 import SearchUI from "../../search/searchUI"
 
 const ControlPanel = ({
@@ -82,41 +82,46 @@ const ControlPanel = ({
               <label htmlFor={layer.id} className="form-check-label">
                 {layer.name}
               </label>
-              {/* Icona per aprire il modal con SearchUI */}
-              <button
-                className="btn btn-sm btn-link ms-2"
+              <svg
                 onClick={() => openModal(layer)}
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-search ms-4"
+                viewBox="0 0 16 16"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-search"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                </svg>
-              </button>
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+              </svg>
             </div>
           ))}
         </div>
       )}
       {/* Modal per la ricerca */}
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Filtra Layer"
-        style={{ overlay: { zIndex: 20 }, content: { zIndex: 30 } }}
-      >
-        <h2>Filtra {activeLayer?.name}</h2>
-        <Button onClick={closeModal}>Chiudi</Button>
-        {activeLayer && (
-          <SearchUI
-            fieldList={activeLayer.fieldList}
-            processData={activeLayer.processData}
-          />
-        )}
+      <Modal show={modalIsOpen} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+            </svg>{" "}
+            {activeLayer?.name}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {activeLayer && (
+            <SearchUI
+              fieldList={activeLayer?.fieldList}
+              processData={activeLayer?.processData}
+            />
+          )}
+        </Modal.Body>
       </Modal>
     </StyledControl>
   )
