ff79bc7311577af48cad32698369e95dcc476f73
Julian Bogdani <jbogdani@gmail.com>
2024-10-08 11:24:58 +0200

    Added proptypes for Field

--- src/modules/record/field.js
+++ src/modules/record/field.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { RecordContext } from "./record"
 import getDataFromObj from "../../services/transformers/getDataFromObj"
+import PropTypes from "prop-types"
 
 const Field = ({ name }) => {
   const value = React.useContext(RecordContext)
@@ -10,6 +11,16 @@ const Field = ({ name }) => {
   } else {
     return <>{getDataFromObj(value, name)}</>
   }
+}
+
+Field.propTypes = {
+  /**
+   * Array containing the index or the indices of the data to return
+   * Required
+   * Example: data = {"one": "One Value", "two": ["Two value #1", "Two value 2"]}
+   * name: ["one"] will returjn "One Value" and name: ["two", "1"] will return "Two value 2"
+   */
+  name: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export { Field }
