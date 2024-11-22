5e314a795437662362df1520e2d3219dc3923f35
Julian Bogdani <jbogdani@gmail.com>
2024-11-22 14:21:33 +0100

    Refacored and inline documented

--- src/modules/record/field.js
+++ src/modules/record/field.js
@@ -1,36 +1,52 @@
-import React from "react"
+import React, { useContext } from "react"
+import PropTypes from "prop-types"
 import { RecordContext } from "./record"
 import getDataFromObj from "../../services/transformers/getDataFromObj"
-import PropTypes from "prop-types"
 
+/**
+ * Field component retrieves and displays data from the RecordContext.
+ * It allows for optional transformation of the data before rendering.
+ *
+ * @param {Object} props - The component props.
+ * @param {Array<string>} props.name - An array of strings representing the keys or indices of the data to retrieve.
+ * @param {Function} [props.transformer] - An optional function to transform the retrieved data before rendering.
+ *
+ * @returns {JSX.Element|null} The rendered component or null if no data is available.
+ */
 const Field = ({ name, transformer }) => {
-  const value = React.useContext(RecordContext)
+  // Retrieve the current value from the RecordContext
+  const value = useContext(RecordContext)
 
-  if (value.length < 1) {
-    return <></>
-  } else {
-    const data = getDataFromObj(value, name);
-    if (transformer){
-      return transformer(data);
-    } else {
-      return <>{ typeof data === 'string' ? data : JSON.stringify(data)}</>
-    }
+  // If the value is empty, return null (no rendering)
+  if (!value) {
+    return null // Use null instead of an empty fragment for better performance
   }
+
+  // Get the relevant data based on the provided name
+  const data = getDataFromObj(value, name)
+
+  // Render transformed data if transformer is provided, otherwise render the data directly
+  return transformer ? (
+    transformer(data)
+  ) : (
+    <>{typeof data === "string" ? data : JSON.stringify(data)}</>
+  )
 }
 
+// Define prop types for the Field component
 Field.propTypes = {
   /**
-   * Array containing the index or the indices of the data to return
-   * Required
+   * Array containing the index or the indices of the data to return.
+   * Required.
    * Example: data = {"one": "One Value", "two": ["Two value #1", "Two value 2"]}
-   * name: ["one"] will returjn "One Value" and name: ["two", "1"] will return "Two value 2"
+   * name: ["one"] will return "One Value" and name: ["two", "1"] will return "Two value 2".
    */
   name: PropTypes.arrayOf(PropTypes.string).isRequired,
 
   /**
-   * A function that received data as input and performs transformation 
-   * or any other typo of logic. Can be used, for example, to loop into array of child data.
-   * Optional. By default if output data are not a string JSON.stringify will be applied to returned data.
+   * A function that receives data as input and performs transformation
+   * or any other type of logic. Can be used, for example, to loop into an array of child data.
+   * Optional. By default, if output data is not a string, JSON.stringify will be applied to returned data.
    */
   transformer: PropTypes.func,
 }
