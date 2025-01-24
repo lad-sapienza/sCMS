1a478aff006e5dc5516d73c94115a1870ffaf899
Julian Bogdani <jbogdani@gmail.com>
2025-01-24 14:47:28 +0100

    Inline docs enhanced

--- src/services/sourcePropTypes.js
+++ src/services/sourcePropTypes.js
@@ -1,16 +1,20 @@
-import PropTypes from "prop-types"
-
+import PropTypes from 'prop-types'
 import directusSourceProptypes from "./directus/directusSourceProptypes"
 import path2dataSourceProptypes from "./path2data/path2dataSourceProptypes"
 
+// Define the shape of the sourcePropTypes object
 const sourcePropTypes = PropTypes.shape({
-  
+  // PropTypes for path2data source
   path2data: path2dataSourceProptypes,
   
+  // PropTypes for directus source
   directus: directusSourceProptypes,
 
+  // PropTypes for custom API source
   customApi: PropTypes.shape({
+    // Function to format the URL
     formatUrl: PropTypes.func.isRequired,
+    // Function to parse the response
     parseResponse: PropTypes.func.isRequired,
   })
 })
