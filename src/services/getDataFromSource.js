20ee46be50d225447a1f6b340a4c49d0e1dda569
Julian Bogdani <jbogdani@gmail.com>
2025-01-24 14:54:32 +0100

    Refactor getDataFromSource function for improved readability and consistency

--- src/services/getDataFromSource.js
+++ src/services/getDataFromSource.js
@@ -2,27 +2,23 @@ import sourcePropTypes from "./sourcePropTypes.js"
 import DirectusService from "./directus/directus.js"
 import Path2DataService from "./path2data/path2data.js"
 
-const getDataFromSource = async ({
-  path2data,
-  directus,
-  customApi,
-}) => {
+const getDataFromSource = async ({ path2data, directus, customApi }) => {
   let sourceUrl
   let options = {}
 
   // path2data source
   if (path2data) {
-    const p2tRet = Path2DataService.formatUrl(path2data);
+    const p2tRet = Path2DataService.formatUrl(path2data)
     sourceUrl = p2tRet.sourceUrl
     options = p2tRet.options
-  // Directus source
+    // Directus source
   } else if (directus) {
     const dirRet = DirectusService.formatUrl(directus)
     sourceUrl = dirRet.sourceUrl
     options = dirRet.options
-  
-  // CustomApi source
-  } else  if (customApi && customApi.formatUrl) {
+
+    // CustomApi source
+  } else if (customApi && customApi.formatUrl) {
     const customRet = customApi.formatUrl()
     sourceUrl = customRet.sourceUrl
     options = customRet.options
@@ -33,16 +29,13 @@ const getDataFromSource = async ({
   try {
     const response = await fetch(sourceUrl, options)
 
-    if (directus){
-      return await DirectusService.parseResponse(response, directus.geoField);
-    
+    if (directus) {
+      return await DirectusService.parseResponse(response, directus.geoField)
     } else if (path2data) {
       return await Path2DataService.parseResponse(response, path2data.path)
-    
     } else if (customApi && customApi.parseResponse) {
       return await customApi.parseResponse(response, customApi.geoField)
     }
-
   } catch (error) {
     // console.log(error)
     throw Error(error)
