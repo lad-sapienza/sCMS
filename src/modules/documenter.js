137d14dc922dfdc360e3e1df8f6190e05636da14
Julian Bogdani <jbogdani@gmail.com>
2025-01-26 11:06:39 +0100

    Enhanced Documenter

--- src/modules/documenter.js
+++ src/modules/documenter.js
@@ -1,22 +1,18 @@
 import React from "react"
-import jsxToString from "react-element-to-jsx-string"
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism"
 
-
-const Documenter = ({ children }) => {
-  const componentCode = jsxToString(children)
-
+const Documenter = ({ rawCode, children }) => {
   return (
     <div className="row">
       <div className="col-sm-4">
         <SyntaxHighlighter language="jsx" style={okaidia}>
-          {componentCode}
-          </SyntaxHighlighter>
+          {rawCode}
+        </SyntaxHighlighter>
       </div>
       <div className="col-sm-8">{children}</div>
     </div>
   )
 }
 
-export {Documenter}
+export { Documenter }
