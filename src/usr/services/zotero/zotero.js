82a6237324e4dc746d88b72aff7f20a100c1c682
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2025-03-24 05:29:12 +0100

    Update zotero.js

--- src/usr/services/zotero/zotero.js
+++ src/usr/services/zotero/zotero.js
@@ -1,18 +1,10 @@
 import formatUrl from "./formatUrl"
 import form2querystring from "./form2querystring"
-import parseResponse from "./parseResponse" // la versione base
-import parseResponseOnto from "./parseResposeOnto" // la versione con ontologia
+import parseResponse from "./parseResponse" 
 
-// 1) Servizio base (solo ricerca testuale)
+
 export const ZoteroService = {
   formatUrl,
   parseResponse,
-  form2querystring,
-}
-
-// 2) Servizio “geo” (fusione con ontologia)
-export const ZoteroGeoService = {
-  formatUrl,
-  parseResponse: parseResponseOnto,
   form2querystring,
 }
