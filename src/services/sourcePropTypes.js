09cbf4e1da7522c2f120986887305ddbaefd7dda
Julian Bogdani <jbogdani@gmail.com>
2025-08-18 08:09:49 +0200

    feat: add geojson prop types to sourcePropTypes definition

--- src/services/sourcePropTypes.js
+++ src/services/sourcePropTypes.js
@@ -6,7 +6,24 @@ import path2dataSourceProptypes from "./path2data/path2dataSourceProptypes"
 const sourcePropTypes = PropTypes.shape({
   // PropTypes for path2data source
   path2data: path2dataSourceProptypes,
-  
+
+  geojson: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string,
+    features: PropTypes.arrayOf(
+      PropTypes.shape({
+        type: PropTypes.string.isRequired,
+        properties: PropTypes.object.isRequired,
+        geometry: PropTypes.shape({
+          type: PropTypes.string.isRequired,
+          coordinates: PropTypes.oneOfType([
+            PropTypes.arrayOf(PropTypes.number),
+            PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)),
+          ]).isRequired,
+        }).isRequired,
+      })
+    ),
+  }).isRequired,
   // PropTypes for directus source
   directus: directusSourceProptypes,
 
