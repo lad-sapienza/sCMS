7c5bfb57f0a392fa27214d9adea19b6fafc171d4
Julian Bogdani <jbogdani@gmail.com>
2026-02-05 09:48:50 +0100

    feat: Remove CustomComponent and DataTbWrapper components to streamline codebase

--- usr/components/DataTbWrapper.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * DataTb Wrapper
- * 
- * Re-exports the DataTb component from core so it can be properly bundled
- * for client-side hydration by Astro
- */
-
-export { DataTb } from '../../core/components/DataTb';
-export type { DataTbProps, ColumnConfig, SourceConfig } from '../../core/components/DataTb/types';
